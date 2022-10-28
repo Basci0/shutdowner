790da0ebb4ad3fe98e401530c23bdf42ad28c2d4
Basci0 <88885656+Basci0@users.noreply.github.com>
2022-10-28 16:31:35 +0200

    Update main.go

--- main.go
+++ main.go
@@ -3,33 +3,44 @@ package main
 import (
 	"fmt"
 	"os/exec"
-	"strings"
-	"time"
+
+	"github.com/pterm/pterm"
 )
 
-func main() {
-
-	for s := 1; s < 11; s++ {
-		time.Sleep(1 * time.Second)
-		fmt.Printf("Shutdown in %v\n", s-11)
-
-	}
-//shutdown
+func shutdown() {
 	if err := exec.Command("cmd", "/C", "shutdown", "/s").Run(); err != nil {
 		fmt.Println("Failed to shutdown:", err)
 	}
-  //if user types n:
-	var result string
-	fmt.Println("Type n for undo.")
-	fmt.Scan(&result)
-	if strings.EqualFold("n", "N") {
-    //cancel shutdown
-		if err := exec.Command("cmd", "/C", "shutdown", "/a").Run(); err != nil {
-			fmt.Println("Failed to cancel:", err)
-
-		}
-	}
 
 }
-//shutdown -r for running
-//shutdown -a for cancelling
+func restart() {
+	if err := exec.Command("cmd", "/C", "shutdown", "/r"); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	//	options := []string{"Shutdown", "Restart"}
+
+	//switch options{
+	//case "Shutdown":
+
+	//case "Restart":
+	//	exec.Command("cmd", "/C", "shutdown", "/r")
+	//}
+
+	a, _ := pterm.DefaultInteractiveMultiselect.
+		WithOptions([]string{"shutdown", "restart"}).
+		Show()
+	//confirm for leaving a, _ := pterm.DefaultInteractiveConfirm.Show()
+
+	pterm.DefaultSpinner.Start()
+
+	pterm.Info.Println("Updating the system..")
+	pterm.Info.Println("Waiting for a answer..")
+
+	pterm.DefaultSpinner.Stop()
+	pterm.Info.Println("%T", a)
+	shutdown()
+}
+
